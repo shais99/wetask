2e7b047db25d0a18a51e0a5182e6261dfd50c656
meshicohenn <meshicohenn@gmail.com>
2020-05-20 18:37:30 +0300

    add handleChange

--- frontend/src/cmps/BoardAdd.jsx
+++ frontend/src/cmps/BoardAdd.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { saveBoard } from '../store/actions/boardActions'
 
-
-export default class BoardAdd extends React.Component {
+class BoardAdd extends React.Component {
     state = {
         board: {
             _id: "101",
-            title: null
+            title: ''
         }
     }
+
+    handleChange = (ev) => {
+        let { name, value } = ev.target;
+        value = ev.target.type === 'number' ? parseInt(value) : value;
+        this.setState(prevState => ({ board: { ...prevState.board, [name]: value } }))
+    };
+
+    onHandleSubmit = (ev) => {
+        ev.preventDefault()
+        const { board } = this.state
+        this.props.saveBoard(board)
+        this.setState({ board: '' })
+    };
 
 
     render() {
@@ -20,13 +34,24 @@ export default class BoardAdd extends React.Component {
                         <button className="close-modal">X</button>
                     </div>
                     <div className="flex">
-                        <input name="title" type="text" className="borad-title" value={board.title} placeholder="Add board title" />
+                        <input name="title" type="text" className="borad-title" value={board.title}
+                            placeholder="Add board title" onChange={this.handleChange} />
                     </div>
                     <div className="modal-footer flex justify-end">
-                        <button className="create-board-btn btn"> Create Board</button>
+                        <button className="create-board-btn btn" onClick={this.onHandleSubmit}> Create Board</button>
                     </div>
                 </div>
             </div>
         </>
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+    };
+};
+const mapDispatchToProps = {
+    saveBoard
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoardAdd);
