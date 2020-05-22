538c87e593171aef5025e55d9a2e6e41e09d4ed1
meshicohenn <meshicohenn@gmail.com>
2020-05-22 18:09:16 +0300

    labels is ready

--- frontend/src/cmps/LabelsPicker.jsx
+++ frontend/src/cmps/LabelsPicker.jsx
@@ -3,40 +3,41 @@ import React, { Component } from 'react'
 
 export default class LabelsPicker extends Component {
     state = {
-        labels: [],
+        labels: [{ title: 'small', color: '#61bd4f' }, { title: 'error', color: '#f2d600' },
+        { title: 'design', color: '#ff9f1a' }, { title: 'feature', color: '#eb5a46' },
+        { title: 'bug', color: '#c377e0' }, { title: 'warning', color: '#0079bf' }]
     }
 
     isLabelChecked = (labelName) => {
         const currCard = this.props.getCurrCard();
-        console.log('currCard',currCard);
-        if(!currCard.labels) currCard.labels = [];
-        if (currCard.labels.find(label => labelName === label)) return true
+        console.log('currCard', currCard);
+        if (!currCard.labels) currCard.labels = [];
+        if (currCard.labels.find(label => labelName === label.title)) return true
         return false
     }
 
+    // labelTitle(title){
+    //     switch (title) {
+    //         case 'value':
+                
+    //             break;
+        
+    //         default:
+    //             break;
+    //     }
+
+    // }
+
     render() {
-        const { addLabel, onToggleAction } = this.props
+        const { addLabel } = this.props
         const { isLabelChecked } = this
-        return <div className="labels-container">
-            <div className="labels-header flex space-between">
-                <h3>Labels</h3>
-                <button className="close-label" onClick={()=>onToggleAction('label')}>X</button>
-            </div>
-            <div className="label-item" style={{ backgroundColor: "#61bd4f" }}
-                onClick={() => addLabel('small')}>Small {isLabelChecked('small') ? 'hi' : ''}</div>
-
-
-
-            <div className="label-item" style={{ backgroundColor: "#f2d600" }}
-                onClick={() => addLabel('error')}>Error</div>
-            <div className="label-item" style={{ backgroundColor: "#ff9f1a" }}
-                onClick={() => addLabel('design')}>Design</div>
-            <div className="label-item" style={{ backgroundColor: "#eb5a46" }}
-                onClick={() => addLabel('feature')}>Feature</div>
-            <div className="label-item" style={{ backgroundColor: "#c377e0" }}
-                onClick={() => addLabel('bug')}>Bug</div>
-            <div className="label-item" style={{ backgroundColor: "#0079bf" }}
-                onClick={() => addLabel('warning')}>Warning</div>
-        </div >
+        return <>
+            {this.state.labels.map(label => {
+                return <div className="label-item flex space-between" style={{ backgroundColor: label.color }}
+                    onClick={() => addLabel({ title: label.title, color: label.color })}>
+                    {label.title} {isLabelChecked(label.title) ? <img src="/assets/img/icon-checked.png" /> : ''}
+                </div>
+            })}
+        </>
     }
 }
