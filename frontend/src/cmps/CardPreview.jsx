7f02ee1a91a578b81b7ce4ff432e21874e27878d
taltara <taltarablus@gmail.com>
2020-05-23 11:50:57 +0300

    Advanced in card-preview info show, design and layout

--- frontend/src/cmps/CardPreview.jsx
+++ frontend/src/cmps/CardPreview.jsx
@@ -3,12 +3,40 @@ import React from 'react';
 export function CardPreview(props) {
 
     const { card, innerRef, provided, style } = props;
+    const showInfo = (card.comments.length || card.description !== '');
+    console.log(card);
     return (
-        <div className="card-preview" ref={innerRef} style={style}
-            {...provided.draggableProps} {...provided.dragHandleProps} >
-            {props.title}
-        </div>
-        // TODO: add dynamic info show for card details (comments icon, description, todos)
+        <>
+            <div className="card-preview" ref={innerRef} style={style}
+                {...provided.draggableProps} {...provided.dragHandleProps} >
+                {props.title}
+
+                {(showInfo)
+                    ?
+                    <div className="card-preview-info flex align-center space-start">
+                        {(card.description !== '')
+                            ?
+                            <span className="preview-info-span flex align-center">
+                                <img className="preview-info-img" src="/assets/img/description.png" />
+                            </span>
+                            :
+                            null
+                        }
+                        {(card.comments.length)
+                            ?
+                            <span className="preview-info-span flex align-center">
+                                <img className="preview-info-img" src="/assets/img/comments.png" />
+                                <p className="preview-info-count">{card.comments.length}</p>
+                            </span>
+                            :
+                            null
+                        }
+                    </div>
+                    : null}
+            </div>
+
+
+        </>
     )
 
 }
