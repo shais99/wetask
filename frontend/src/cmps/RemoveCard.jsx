d17fd67ae4358e68f3cd7e4fdbbdb0584fab7b9c
meshicohenn <meshicohenn@gmail.com>
2020-06-01 08:57:29 +0300

    pull

--- frontend/src/cmps/RemoveCard.jsx
+++ frontend/src/cmps/RemoveCard.jsx
@@ -6,8 +6,8 @@ export default function ReomveCard(props) {
         <div className="remove-board-container remove-card">
             <div className="remove-board-title remove-card-title flex align-center space-between">
                 {`Remove ${(type && type === 'stack') ? 'list' : 'card'}`}
-                <img src="/assets/img/close.png" className="close-icon" onClick={() => props.onToggleRemoveStack()} alt="" />
-            </div>
+                <img src="/assets/img/close.png" className="close-icon" onClick={props.onToggleRemoveCard} alt="" />
+            </div>d
             <p>Are you sure?</p>
             {(type && type === 'stack') ?
                 <button className="btn btn-danger btn-remove-card" onClick={() => props.onRemoveStack()}>Yes, remove!</button>
